3620e63df5bf8f794173904259acf174e63a6e35
emil <emil.dekeyser@student.ucll.be>
2018-02-18 11:12:11 +0100

    'af'

---
+++ OOO_Part1/refactor/src/cipher_refactor/Cipher.java
@@ -0,0 +1,6 @@
+package cipher_refactor;
+
+public abstract class Cipher 
+{
+	public abstract String encode(String plaintext);
+}
---
+++ OOO_Part1/refactor/src/cipher_refactor/Mirroring.java
@@ -0,0 +1,20 @@
+package cipher_refactor;
+
+public class Mirroring extends Cipher 
+{
+	@Override
+	public String encode(String plaintext) 
+	{
+		String encoded = "";
+		char[] oneWay = plaintext.toCharArray();
+		int length = oneWay.length;
+		char[] otherWay = new char[length];
+		for(int i = 0, j = length - 1; j >= 0; i++, j--)
+		{
+			otherWay[j] = oneWay[i];
+		}
+		encoded = new String(otherWay);
+		return encoded;
+	}
+
+}
